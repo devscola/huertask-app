903b1b642b415b9420f7a092d6efeaa65015cb60
miguelonga <miguelmundiaragones@gmail.com>
2016-11-01 13:12:11 +0100

    change default pages to tasks page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
-
+import { Tasks } from '../pages/tasks/tasks';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +10,7 @@ import { Page2 } from '../pages/page2/page2';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = Page1;
+  rootPage: any = Tasks;
 
   pages: Array<{title: string, component: any}>;
 
@@ -21,8 +19,7 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: 'Tasks', component: Tasks }
     ];
 
   }
---
+++ src/pages/tasks/tasks.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+interface Task {
+	title: string,
+	date: string,
+	category: string,
+	people_left: number
+}
+
+@Component({
+  selector: 'tasks',
+  templateUrl: 'tasks.html'
+})
+export class Tasks {
+
+public list: Task[];
+
+  constructor(public navCtrl: NavController) {
+    this.list = [
+		{
+			title: "Regar parcela",
+			date: "2016/10/31",
+			category: "general",
+			people_left: 2
+		},
+		{
+			title: "Recoger tomates",
+			date: "2016/12/07",
+			category: "cosecha",
+			people_left: 1
+		},
+		{
+			title: "Barrer entrada",
+			date: "2016/12/09",
+			category: "limpieza",
+			people_left: 4
+		},
+		{
+			title: "Traer leña",
+			date: "2016/12/12",
+			category: "general",
+			people_left: 1
+		},
+		{
+			title: "Quitar malas hierbas",
+			date: "2016/12/02",
+			category: "limpieza",
+			people_left: 5
+		},
+		{
+			title: "Ampliar parcelas zona oeste",
+			date: "2016/12/13",
+			category: "general",
+			people_left: 9
+		}
+	];
+  }
+
+}
