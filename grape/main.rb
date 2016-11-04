84990411d35afc821179c46c2eb3e80ce3106556
Interdictor <interdictor.dsg@gmail.com>
2016-11-04 19:33:46 +0100

    Test: tasks endpoint returns only future tasks

--- grape/main.rb
+++ grape/main.rb
@@ -1,5 +1,38 @@
 require 'grape'
 
+TASKS = [
+          {
+            title: "Recoger tomates",
+            date: "2012-01-01 00:00:00",
+            category: "cosecha",
+            people_left: 1
+          },
+          {
+            title: "Barrer entrada",
+            date: "2016-12-09 00:00:00",
+            category: "limpieza",
+            people_left: 4
+          },
+          {
+            title: "Traer leña",
+            date: "2016-12-12 00:00:00",
+            category: "general",
+            people_left: 1
+          },
+          {
+            title: "Quitar malas hierbas",
+            date: "2016-12-02 00:00:00",
+            category: "limpieza",
+            people_left: 5
+          },
+          {
+            title: "Ampliar parcelas zona oeste",
+            date: "2016-12-13 00:00:00",
+            category: "general",
+            people_left: 9
+          }
+        ]
+
 module Huertask
   class API < Grape::API
 
@@ -9,40 +42,18 @@ module Huertask
 
     resource :tasks do
       get "/" do
-        [
-          {
-            title: "Recoger tomates",
-            date: "2016/12/07",
-            category: "cosecha",
-            people_left: 1
-          },
-          {
-            title: "Barrer entrada",
-            date: "2016/12/09",
-            category: "limpieza",
-            people_left: 4
-          },
-          {
-            title: "Traer leña",
-            date: "2016/12/12",
-            category: "general",
-            people_left: 1
-          },
-          {
-            title: "Quitar malas hierbas",
-            date: "2016/12/02",
-            category: "limpieza",
-            people_left: 5
-          },
-          {
-            title: "Ampliar parcelas zona oeste",
-            date: "2016/12/13",
-            category: "general",
-            people_left: 9
-          }
-        ]
+        future_tasks
       end
     end
 
+    helpers do
+      def future_tasks
+        TASKS.select {|task| future_task?(task)}
+      end
+
+      def future_task? task
+        task[:date] >= Time.now.utc
+      end
+    end
   end
 end
--- grape/spec/routes/tasks_spec.rb
+++ grape/spec/routes/tasks_spec.rb
@@ -8,14 +8,30 @@ describe Huertask::API do
     Huertask::API
   end
 
-  describe "GET /api/tasks" do
+  let(:request_time) { Time.now.utc }
 
+  describe "GET /api/tasks" do
     subject(:tasks) { JSON.parse(last_response.body) }
 
     it "returns ok" do
       get "/api/tasks"
 
       expect(last_response).to be_ok
+    end
+
+    it "returns future tasks" do
+      get "api/tasks?category=future"
+
+      expect(last_response).to be_ok
+      expect(past_tasks).to be_empty
+    end
+
+    def past_tasks
+      tasks.select {|task| past_task?(task)}
+    end
+
+    def past_task? task
+      task["date"] < request_time
     end
   end
 end
