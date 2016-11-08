3c95b5408e1b0d03de33a334ccc74e98954cd1f3
Pardiez <david@davidpena.es>
2016-11-08 11:12:39 +0100

    add i18n to tasks list

--- ionic/src/app/app.module.ts
+++ ionic/src/app/app.module.ts
@@ -3,12 +3,25 @@ import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Tasks } from '../pages/tasks/tasks';
 
+import { HttpModule, Http } from '@angular/http';
+import { TranslateModule, TranslateLoader, TranslateStaticLoader } from 'ng2-translate';
+
+export function createTranslateLoader(http: Http) {
+    return new TranslateStaticLoader(http, './assets/i18n', '.json');
+}
+
 @NgModule({
   declarations: [
     MyApp,
     Tasks
   ],
   imports: [
+    HttpModule,
+    TranslateModule.forRoot({
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [Http]
+    }),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
