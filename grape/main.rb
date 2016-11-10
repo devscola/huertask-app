5bdf29f0840122d7b8289f1214f49a08f439b8ea
Pardiez <david@davidpena.es>
2016-11-10 22:47:16 +0100

    add datamapper

--- grape/main.rb
+++ grape/main.rb
@@ -1,7 +1,9 @@
 require 'grape'
+require 'data_mapper'
 
 require_relative './data/tasks'
 require_relative './data/users'
+require_relative'./models/task'
 
 module Huertask
   class API < Grape::API
@@ -14,16 +16,25 @@ module Huertask
       get "/" do
         future_tasks
       end
+
+      desc 'Create a new task'
+      post '/' do
+        created_task = Task.create params[:task]
+        created_task
+      end
     end
 
     helpers do
       def future_tasks
-        TASKS.select {|task| future_task?(task)}
+        Task.all.select {|task| future_task?(task)}
       end
 
       def future_task? task
         task[:date] >= Time.now.utc
       end
+
+      DataMapper::setup(:default, ENV['HUERTASK_DB_URL'] || "sqlite3://#{Dir.pwd}/tasks.db")
+      DataMapper.auto_upgrade!
     end
   end
 end
