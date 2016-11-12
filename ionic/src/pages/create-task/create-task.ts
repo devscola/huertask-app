3566d768df2c1748d33dea26c87115ae65c5abbd
miguelonga <miguelmundiaragones@gmail.com>
2016-11-12 18:05:25 +0100

    show toast when create task

--- ionic/src/pages/create-task/create-task.ts
+++ ionic/src/pages/create-task/create-task.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { TaskService } from '../../providers/task.service';
 import { Task } from '../../models/task';
 import { Tasks } from '../tasks/tasks';
@@ -14,7 +14,7 @@ export class CreateTask {
   task = new Task();
   categories;
   submited = false;
-  constructor(public navCtrl: NavController, private taskService: TaskService) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, private taskService: TaskService) {
     this.categories = taskService.categories
   }
 
@@ -25,10 +25,27 @@ export class CreateTask {
   createTask(task: Object){
     this.taskService.createTask(task).subscribe( data => {
       console.log(data);
+      if(data.id !== null){
+        this.presentToast('¡Tarea guardada!', 'success')
+      }else{
+        this.presentToast('No se pudo guardar la tarea', 'danger')
+      }
       return data
     },
     err => console.log(err)
     )
     this.navCtrl.setRoot(Tasks);
+  }
+
+
+  presentToast(message: string, cssClass: string = '') {
+    let toast = this.toastCtrl.create({
+     message: message,
+     duration: 3000,
+     position: 'bottom',
+     cssClass: cssClass
+    });
+
+    toast.present();
   }
 }
