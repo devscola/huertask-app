10cac3c0a297b1cc3488cefe2d5780cd53852f3e
miguelonga <miguelmundiaragones@gmail.com>
2016-11-25 19:47:03 +0100

    test edit task page

---
+++ app/src/pages/edit-task/edit-task.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, async } from '@angular/core/testing';
+import { TestUtils }               from '../../test';
+import { EditTask }             from './edit-task';
+
+
+let fixture: ComponentFixture<EditTask> = null;
+let instance: any = null;
+
+describe('Pages: Edit Task', () => {
+
+  beforeEach(async(() => TestUtils.beforeEachCompiler([EditTask]).then(compiled => {
+    fixture = compiled.fixture;
+    instance = compiled.instance;
+  })));
+
+  it('should create the edit task page', async(() => {
+    expect(instance).toBeTruthy();
+  }));
+
+  it('should has the task information', async(() => {
+    let template = fixture.nativeElement;
+    fixture.detectChanges();
+
+    let title = template.getElementsByTagName('ion-input')[0];
+    let titleValue = title.getAttribute('ng-reflect-model');
+    expect(titleValue).toBe('Tarea numero 3');
+  }));
+});
