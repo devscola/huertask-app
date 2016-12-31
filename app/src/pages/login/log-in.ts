f0bdc622b1d8cef46a00b4b2031fbf53d20c31cd
miguelonga <miguelmundiaragones@gmail.com>
2016-12-31 13:22:44 +0100

    add log-in pages

---
+++ app/src/pages/login/log-in.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PersonService } from '../../providers/person.service';
+import { Tasks } from '../tasks/tasks';
+
+@Component({
+  selector: 'log-in',
+  templateUrl: './log-in.html'
+})
+
+export class LogIn {
+  form;
+
+  constructor(
+   public toastCtrl: ToastController,
+   public navCtrl: NavController,
+   public formBuilder: FormBuilder,
+   public personService: PersonService
+  ){
+    this.form = this.formBuilder.group({
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+  }
+
+  logIn(){
+    let person = this.form.value
+
+    this.personService.logIn(person).subscribe(person => {
+      console.log(person)
+      this.presentToast(("Hola " + person.name), "success")
+      this.navCtrl.setRoot(Tasks)
+    }, err => {
+      this.presentToast("incorrecto", "danger")
+      console.log(err)
+    })
+
+  }
+
+  presentToast(message: string, cssClass: string = '') {
+    let toast = this.toastCtrl.create({
+     message: message,
+     duration: 3000,
+     position: 'bottom',
+     cssClass: cssClass
+    });
+
+    toast.present();
+  }
+}
