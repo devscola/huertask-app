475335d3bd5f8c83ff74969e125a054e6af8433a
miguelonga <miguelmundiaragones@gmail.com>
2017-01-26 19:18:26 +0100

    add search bar to people page

--- app/src/pages/people/people.ts
+++ app/src/pages/people/people.ts
@@ -8,10 +8,11 @@ import { Person } from '../../models/person';
   templateUrl: './people.html'
 })
 export class People {
+  filteredList: Person[];
   list: Person[];
   joined: Person[];
   invited: Person[];
-
+  searching = false;
   tabs = [
     {title: "PEOPLE.JOINED", active: true},
     {title: "PEOPLE.INVITED", active: false}
@@ -25,12 +26,14 @@ export class People {
       this.joined = community.joined;
       this.invited = community.invited;
       this.list = this.joined;
+      this.filteredList = this.list
     });
   }
 
   showTasks(tabTitle){
     if(tabTitle === "PEOPLE.JOINED"){ this.list = this.joined }
     if(tabTitle === "PEOPLE.INVITED"){ this.list = this.invited }
+    this.filteredList = this.list
   }
 
   selectTab(tabTitle) {
@@ -40,6 +43,26 @@ export class People {
     });
     this.showTasks(tabTitle)
   }
+
+  search(){
+    this.searching = true
+  }
+
+  getItems(ev) {
+   this.filteredList = this.list
+   let val = ev.target.value;
+   if (val && val.trim() != '') {
+     this.filteredList = this.list.filter((item) => {
+       return (item.email.toLowerCase().indexOf(val.toLowerCase()) > -1);
+     })
+   }
+ }
+
+ onClear(ev) {
+   this.filteredList = this.list;
+   this.searching = false;
+   ev.target.value = '';
+ }
 
   goToInvitePerson(){
     console.log("Ir a la página de invitar a usuario");
